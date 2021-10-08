524e5d5ce496a1d106dbce57e21e825ebaba6340
112224 <didgns25@naver.com>
2021-10-08 22:16:07 +0900

    10/08

--- python3/17143 낚시왕.py
+++ python3/17143 낚시왕.py
@@ -1,18 +1,23 @@
 input = __import__('sys').stdin.readline
 
-Or, Oc, m = map(int, input().split())
-di = [(), (-1, 0), (1,0), (0, 1), (-1, 0)]
-board = [[-1] * Oc for _ in range(Or)]
-visit = [False] * m
+R, C, M = map(int, input().split())
+di = [(-1, 0), (1,0), (0, 1), (0, -1)]
+
+board = [[-1] * C for _ in range(R)]
+visit = [False] * M
 fish = []
-for i in range(m):
-    r,c,s,d,z = map(int, input().split())
-    fish.append([r-1,c-1,s,d,z])
+for i in range(M):
+    r, c, s, d, z = map(int, input().split())
+    if d in [1, 2]:
+        s %= (R-1)*2
+    else:
+        s %= (C-1)*2
+    fish.append([r-1, c-1, s, d - 1, z])
     board[r-1][c-1] = i
 
 def catch_fish(pos, visit):
     ret = -1
-    for i in range(Or):
+    for i in range(R):
         if board[i][pos] != -1:
             ret = board[i][pos]
             board[i][pos] = 0
@@ -20,43 +25,40 @@ def catch_fish(pos, visit):
             return ret
     return ret
 
-def move_fish(fish, visit, Or, Oc):
-    moved_board = [[-1] * Oc for _ in range(Or)]
+def move_fish(fish, visit):
+    moved_board = [[-1] * C for _ in range(R)]
     for i in range(len(fish)):
         if visit[i]: continue
         r, c, s, d, z = fish[i]
-        dr, dc = di[d]
-        nr, nc = r + dr *s, c + dc * s
-        rr, rc = divmod(nr, Or)
-        if rr % 2 == 0:
-            nr = rc
-        else:
-            nr = Or - 1 - rc
-        cr, cc = divmod(nc, Oc)
-        if cr % 2 == 0:
-            nc = cc
-        else:
-            nc = Oc - 1 - cc
+        nr, nc = r, c
+        for _ in range(s):
+            nr += di[d][0]
+            nc += di[d][1]
+            if 0 <= nr < R and 0 <= nc < C: continue
+            if d == 0: d = 1
+            elif d == 1: d = 0
+            elif d == 2: d = 3
+            elif d == 3: d = 2
+            nr += di[d][0] * 2
+            nc += di[d][1] * 2
+
         if moved_board[nr][nc] != -1:
-            val = moved_board[nr][nc]
-            if fish[val][4] < d:
-                visit[val] = True
-                moved_board[nr][nc] = i
-            else:
+            prev = moved_board[nr][nc]
+            if fish[prev][4] > z:
                 visit[i] = True
                 continue
+            else:
+                visit[prev] = True
         moved_board[nr][nc] = i
         fish[i] = [nr, nc, s, d, z]
     return moved_board
 
 pos = 0
 ans = 0
-while pos < c:
+while pos < C:
     cur = catch_fish(pos, visit)
     if cur != -1:
         ans += fish[cur][4]
-    board = move_fish(fish, visit, Or, Oc)
+    board = move_fish(fish, visit)
     pos += 1
 print(ans)
-
-
