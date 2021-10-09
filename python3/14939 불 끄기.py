9a7be2168642aa5bfc04ea11079ed4ecd5b231d2
112224 <didgns25@naver.com>
2021-10-09 09:03:25 +0900

    10/09

--- python3/14939 불 끄기.py
+++ python3/14939 불 끄기.py
@@ -1,20 +1,38 @@
 input = __import__('sys').stdin.readline
 
 n = 10
-di = [(-1, 0), (0, 0), (1, 0) ,(0, -1), (0, 1)]
-board = [list(input().rstrip()) for _ in range(n)]
-
+board = [0] * n
+di = [(0, 0), (0,1), (1,0), (0,-1),(-1, 0)]
 # 같은 지역은 최대 한 번만
-for i in range(n):
-    for j in range(n):
-        if board[i][j] == 'O':
-            cnt = 0
-            rcnt = 0
-            for dx, dy in di:
-                nx, ny = i + dx, j + dy
-                if 0<=nx<n and 0<=ny<n:
-                    if board[nx][ny] == 'O':
-                        cnt += 1
-                    else:
-                        rcnt += 1
-            if rcnt == 0:
+# 그럼 윗줄, 오른쪽은? => 끄고 지나가야함 무조건
+for i in range(10):
+    cur = input().rstrip()
+    for j, ch in enumerate(cur):
+        if ch == 'O':
+            board[i] |= (1<<j)
+
+def switch(tboard, x, y):
+    for dx, dy in di:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < n:
+            tboard[nx] ^= (1<<ny)
+
+ans = -1
+for cur in range(1<<n):
+    tboard = [0] * 10
+    for i in range(n):
+        tboard[i] = board[i]
+    ret = 0
+    for i in range(n):
+        if cur & (1<<i):
+            switch(tboard, 0, i)
+            ret += 1
+    for i in range(1, n):
+        for j in range(n):
+            if tboard[i-1] & (1<<j):
+                switch(tboard, i, j)
+                ret += 1
+    if tboard[n-1] == 0:
+        if ans == -1 or ans > ret:
+            ans = ret
+print(ans)
